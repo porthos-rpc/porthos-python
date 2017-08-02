36d909de8ff39f08c928e9ac8d4fa15f44260625
Germano Fronza <germano@eventials.com>
2017-08-02 18:48:01 -0300

    Initial commit

---
+++ examples/client.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import os
+
+import porthos
+
+
+def run_example():
+    with porthos.Client(os.environ["AMQP_URL"], "SampleService") as c:
+        try:
+            response = c.call("method").with_timeout(100).with_dict({"foo": "bar"}).sync()
+
+            if response.status_code == porthos.Status.OK:
+                print("Got content type: %s" % response.content_type)
+                print(response.content)
+            else:
+                print("Got status code: %d" % response.status_code)
+        except porthos.TimeoutException:
+            print("TimeoutException")
+
+
+if __name__ == "__main__":
+    run_example()
+
