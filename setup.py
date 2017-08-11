5418e567ec9b372cb35609d68d8c688095b50f7f
Germano Fronza <germano@eventials.com>
2017-08-11 00:55:04 -0300

    Bumps up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='porthos-python',
-    version='0.0.2',
+    version='0.0.3',
     author='Germano Fronza',
     author_email='germano.inf@gmail.com',
     maintainer='Germano Fronza',
